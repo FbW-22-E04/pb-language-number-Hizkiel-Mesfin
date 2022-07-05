2ba413cd833747bad323022d19fb577eaa7341f4
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-07-05 12:13:39 +0200

    modified the js file

---
+++ solution.js
@@ -0,0 +1,15 @@
+//#1
+
+const introSentence = "The age most people want to die at is";
+const age = 120;
+
+console.log(introSentence + " " + age);
+// The + operator is concatenating the the variables
+
+//#2
+
+const integer = "1005";
+console.log(parseInt(integer));
+
+const float = "10.05";
+console.log(parseFloat(float));
